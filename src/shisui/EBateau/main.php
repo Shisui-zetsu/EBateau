46166863428b96e334e99865db398830e3d455e2
Shisui-zetsu <81517483+Shisui-zetsu@users.noreply.github.com>
2021-04-04 10:44:32 +0200

    Create main.php

---
+++ src/shisui/EBateau/main.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace shisui\EBateau;
+
+use shisui\EBateau\entity\Boat as BoatEntity;
+
+use shisui\EBateau\item\Boat as BoatItem;
+
+use shisui\EBateau\listener\EventListener;
+
+use pocketmine\entity\Entity;
+
+use pocketmine\inventory\ShapelessRecipe;
+
+use pocketmine\item\Item;
+
+use pocketmine\item\ItemFactory;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+		public function onLoad(): void{
+
+		Entity::registerEntity(BoatEntity::class, true);
+
+		ItemFactory::registerItem($item = new BoatItem(), true);
+
+		if (!Item::isCreativeItem($item))
+
+			Item::addCreativeItem($item);
+
+	}
+
+	
+
+	public function onEnable() : void{
+
+		$this->getServer()->getCraftingManager()->registerShapelessRecipe(new ShapelessRecipe([
+
+			Item::get(Item::WOODEN_PLANKS, 0, 5),
+
+			Item::get(Item::WOODEN_SHOVEL, 0, 1)
+
+		], [Item::get(Item::BOAT, 0, 1)]));
+
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+
+	}
+
+}
